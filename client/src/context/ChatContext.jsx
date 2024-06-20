691815a5567ca063aff1d9ada994ac8dec272be9
Harshey09 <111156962+Harshey09@users.noreply.github.com>
2024-06-20 17:33:45 +0530

    Update ChatContext.jsx

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -26,7 +26,7 @@ export const ChatContextProvider = ({children, user}) => {
     
     // initializing socket
     useEffect(()=>{
-        const newSocket = io("https://localhost:3000");
+        const newSocket = io("http://localhost:3000");
         setSocket(newSocket);
         return ()=>{
             newSocket.disconnect();
